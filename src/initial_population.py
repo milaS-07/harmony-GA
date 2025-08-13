ad311d74abd4e200db752cd48c152df1854cd552
milaS-07 <mila.starovic07@gmail.com>
2025-08-13 22:37:50 +0200

    no_voice_overlap radi bolje ali i dalje gresi

--- src/initial_population.py
+++ src/initial_population.py
@@ -4,7 +4,6 @@ from music_converter import midi_to_chromosome, chromosome_to_midi
 from constraints import RANGES
 
 
-
 def generate_initial_population(soprano: list, key: key.Key):
     population = []
 
@@ -15,8 +14,6 @@ def generate_initial_population(soprano: list, key: key.Key):
 
     return population
 
-
-
 def generate_individual(soprano: list, key: key.Key):
     individual = []
     for i in range(len(soprano)):
@@ -25,23 +22,56 @@ def generate_individual(soprano: list, key: key.Key):
 
         upper_limit = soprano_midi
 
-        for (low, high) in RANGES:
+        for j, (low, high) in enumerate(RANGES):
             voice_high = min(high, upper_limit)
             voice_low = low
 
             down_chrom = midi_to_chromosome(voice_low, key)[0]
             up_chrom = midi_to_chromosome(voice_high, key)[0]
 
-            second_num = 0
+            alteration = 0
+
             while True:
-                first_num = random.randint(down_chrom, up_chrom)
-                if chromosome_to_midi([first_num, second_num], key) <= upper_limit:
-                    moment.append([first_num, second_num])
-                    upper_limit = chromosome_to_midi([first_num, second_num], key)
-                    break
+                tone = random.randint(down_chrom, up_chrom)
+                if right_voice_ranges(tone, alteration, upper_limit, key):
+                    if i == 0 or no_voice_overlap(tone, alteration, individual[i-1], j + 1, i, soprano):
+                        upper_limit = chromosome_to_midi([tone, alteration], key)
+
+                        moment.append([tone, alteration])
+                        break
+
 
         individual.append(moment)
 
     return individual
 
+def right_voice_ranges(tone: int, alteration: int, upper_limit: int, key: key.Key):
+    return chromosome_to_midi([tone, alteration], key) <= upper_limit
+
+def no_voice_overlap(tone: int, alteration: int, last_moment: list, voice_ind: int, moment_ind: int, soprano: list):
+    whole_last_moment = [soprano[moment_ind - 1]]
+    whole_last_moment.extend(last_moment)
+
+    last_moment_tone_up = whole_last_moment[voice_ind - 1][0]
+    last_moment_alteration_up = whole_last_moment[voice_ind - 1][1]
+
+    if voice_ind == 3:
+        if tone > last_moment_tone_up:
+            return False
+        elif tone == last_moment_tone_up:
+            return alteration <= last_moment_alteration_up
+        else:
+            return True
+    else:
+        last_moment_tone_down = whole_last_moment[voice_ind + 1][0]
+        last_moment_alteration_down = whole_last_moment[voice_ind + 1][1]
+
+        if last_moment_tone_down < tone < last_moment_tone_up:
+            return True
+        elif last_moment_tone_down == tone:
+            return last_moment_alteration_down <= alteration
+        elif tone == last_moment_tone_up:
+            return last_moment_alteration_up >= alteration
+        else:
+            return False
 
